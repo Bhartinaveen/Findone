2a97d8f4781496715ce06d5ef7ba1c92caeb2317
Naveen Bharti <bhartinaveen943@gmail.com>
2026-03-18 07:48:57 +0530

    Security: Remove sensitive/temporary files and add .gitignore

--- backend/test_ratings_quick.js
+++
@@ -1,8 +0,0 @@
-const { scrapeRatingDistribution } = require('./scraper/reviewScraper');
-const url = 'https://www.flipkart.com/apple-iphone-15-black-128-gb/p/itm6ac6485515ae4'; // test url
-(async () => {
-    console.log("Testing Flipkart iPhone 15");
-    const ratings = await scrapeRatingDistribution(url);
-    console.log("Result:", ratings);
-    process.exit(0);
-})();
